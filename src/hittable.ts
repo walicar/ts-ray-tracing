3f29759a55e7332b79df3c0674bf185138909920
Will Alicar <will@alicar.me>
2025-07-09 19:29:18 -0700

    feat: implement more of the sphere class

--- src/hittable.ts
+++ src/hittable.ts
@@ -1,12 +1,30 @@
-import type { vec3 } from "gl-matrix";
+import { vec3 } from "gl-matrix";
 import type ray from "./ray";
 
 export default abstract class Hittable {
     abstract hit(r: ray, tmin: number, tmax: number, hitRecord: HitRecord): boolean;
 }
 
-export interface HitRecord {
+export class HitRecord {
     point: vec3;
     normal: vec3;
-    t: number
+    t: number;
+    isFrontFace: boolean;
+
+    constructor(point:vec3, normal:vec3, t:number) {
+        this.point = point;
+        this.normal = normal;
+        this.t = t;
+        this.isFrontFace = false;
+    }
+
+    /**
+     * set isFrontFace and the normal
+     * @param r 
+     * @param outwardNormal 
+     */
+    setFaceNormal(r: ray, outwardNormal: vec3) {
+        this.isFrontFace = vec3.dot(r.dir, outwardNormal) < 0;
+        this.normal = this.isFrontFace ? outwardNormal : vec3.negate(vec3.create(), outwardNormal);
+    }
 }
--- src/sphere.ts
+++ src/sphere.ts
@@ -1,6 +1,6 @@
-import type { vec3 } from "gl-matrix";
+import { vec3 } from "gl-matrix";
 import Hittable, { type HitRecord } from "./hittable";
-import type ray from "./ray";
+import ray from "./ray";
 
 export default class Sphere extends Hittable {
     private center: vec3;
@@ -12,8 +12,31 @@ export default class Sphere extends Hittable {
         this.radius = radius;
     }
 
-    hit(r: ray, tmin: number, tmax: number, hitRecord: HitRecord): boolean {
-        throw new Error("Method not implemented.");
-    } 
+    hit(ray: ray, tmin: number, tmax: number, rec: HitRecord): boolean {
+        // direction from ray origin to sphere center
+        const qc = vec3.sub(vec3.create(), this.center, ray.orig);
+        const a = vec3.dot(ray.dir, ray.dir);
+        const h = vec3.dot(ray.dir, qc);
+        const c = vec3.dot(qc, qc) - (this.radius ** 2);
+        const discriminant = (h ** 2) - (a * c);
 
+        if (discriminant < 0) return false;
+
+        const sqrtd = Math.sqrt(discriminant);
+        let root = (h - sqrtd) / a
+        if (tmin > root || root < tmax) {
+            root = (h + sqrtd) / a;
+            if (tmin > root || root < tmax) {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.point = ray.at(rec.t);
+        const outwardNormal = vec3.sub(vec3.create(), rec.point, this.center);
+        const checkNormal = vec3.sub(vec3.create(), ray.orig, this.center);
+        console.log("check if true", vec3.equals(outwardNormal, checkNormal));
+        rec.setFaceNormal(ray, outwardNormal);
+        return true;
+    }
 }
